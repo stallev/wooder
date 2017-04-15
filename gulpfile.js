1f7277335ca372248a1d895ad325512c79af6494
stallev <stwork87@gmail.com>
2017-04-15 21:41:51 +0300

    corrected some mistakes

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('serve', ['style'], function() {
     cors: true,
     ui: false
   });
-
+  
   gulp.watch('sass/**/*.{scss,sass}', ['style']);
   gulp.watch('*.html', ['copyHtml']);
   gulp.watch('build/*.html').on('change', server.reload);
@@ -49,10 +49,15 @@ gulp.task('copy', function(){
     'js/**',
     'img/*.svg',
     '*.html'
-    ], {
-      base: '.'
-    })
+  ], {
+    base: '.'
+  })
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('copyBootstrapJS', function(){
+  return gulp.src(['node_modules/bootstrap-sass/assets/javascripts/*.js'])
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('clean', function(){
@@ -61,20 +66,20 @@ gulp.task('clean', function(){
 
 gulp.task('copyHtml', function(){
   return gulp.src(['*.html'], {base: '.'})
-  .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('images', function() {
   return gulp.src('img/**/*.{jpg,png,gif}')
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true})
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
     ]))
-  .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('build', function(fn){
-  run('clean', 'copy', 'images', 'style', fn);
+  run('clean', 'copy', 'copyBootstrapJS', 'images', 'style', fn);
 });
 
 gulp.task('server', function(){
@@ -82,5 +87,5 @@ gulp.task('server', function(){
 });
 gulp.task('deploy', function() {
   return gulp.src('build/**/*')
-  .pipe(ghPages());
+    .pipe(ghPages());
 });
